18be70b9f2a0406e42ad46692178802828aa4349
majiabin <majiabin@jianke.com>
2020-01-11 17:09:09 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  head: [
+    ['link', { rel: 'icon', sizes: 48,  href: '/public/favicon.ico' }]
+  ],
+  markdown: {
+    lineNumbers: true
+  }
+}
