3c468bd3e29298dc15b7e0842deec1d2650aa8c9
majiabin <majiabin@jianke.com>
2020-01-11 17:18:25 +0800

    edit title

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: 'Hello VuePress',
-  description: 'Just playing around',
+  title: '好吃的鳕鱼堡',
+  description: '好吃的鳕鱼堡',
   head: [
     ['link', { rel: 'icon', sizes: 48,  href: '/public/favicon.ico' }]
   ],
