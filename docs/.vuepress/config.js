90f56c0cd297ea238a81aa4d602a72b1511555a1
majiabin <majiabin@jianke.com>
2020-05-22 19:03:17 +0800

    调整配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,25 @@
 module.exports = {
-  title: '好吃的鳕鱼堡',
-  description: '好吃的鳕鱼堡',
-  head: [
-    ['link', { rel: 'icon', sizes: 48,  href: '/public/favicon.ico' }]
-  ],
+  title: "main title",
+  description: "description title",
+  themeConfig: {
+    lastUpdated: 'Last Updated', // string | boolean
+    sidebarDepth: 2,
+    sidebar: 'auto',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      {
+        text: 'Languages',
+        items: [
+          { text: 'Chinese', link: '/language/chinese' },
+          { text: 'English', link: '/language/english' }
+        ]
+      },
+      { text: 'External', link: 'https://www.baidu.com' },
+    ]
+  },
+  head: [["link", { rel: "icon", sizes: 48, href: "/public/favicon.ico" }]],
   markdown: {
-    lineNumbers: true
-  }
-}
+    lineNumbers: true,
+  },
+};
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,1 @@
-# Hello VuePress!
+test world
